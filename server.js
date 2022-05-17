8febc9dea7d19657351c9b9ee6cfa9cf3df76871
Amine-me <55441483+Amine-me@users.noreply.github.com>
2022-05-17 13:52:16 +0100

    dockerFile

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:4200",
 };
 
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json());  /* bodyParser.json() is deprecated */
+app.use(express.json()); /* bodyParser.json() is deprecated */
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+app.use(
+  express.urlencoded({ extended: true })
+); /* bodyParser.urlencoded() is deprecated */
 
 const db = require("./app/models");
 
